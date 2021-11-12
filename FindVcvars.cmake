ab2c590fa8fda3b3a4bbf3c0355d829a349da327
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2021-11-12 09:25:23 -0500

    README: Mention that there is no support for "Microsoft Visual C++ Compiler for Python 2.7"

--- FindVcvars.cmake
+++ FindVcvars.cmake
@@ -100,8 +100,6 @@ This module also defines the following functions
 
 cmake_minimum_required(VERSION 3.5)
 
-# TODO Support lookup of "Microsoft Visual C++ Compiler for Python 2.7 (x86, amd64)"
-
 # Global variables used only in this script (unset at the end)
 set(_Vcvars_MSVC_ARCH_REGEX "^(32|64)$")
 set(_Vcvars_MSVC_VERSION_REGEX "^[0-9][0-9][0-9][0-9]$")
